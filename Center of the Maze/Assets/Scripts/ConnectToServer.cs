6e03c4471c7bd4f72bc2a3671ba8dac5275a3f13
Redeyedfigure <dmarsh735@gmail.com>
2023-04-18 07:25:08 +0700

    Changes

---
+++ Center of the Maze/Assets/Scripts/ConnectToServer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Photon.Pun;
+
+public class ConnectToServer : MonoBehaviourPunCallbacks
+{
+
+    void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+}
